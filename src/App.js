626d19ac0e88286dd5f4db575dbb5bf3ecf089c2
ck6652265 <ck6652265@gmail.com>
2026-08-07 12:59:29 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import { common, protectedRoutes, publicRoutes, underConstruction } from './rout
 
 function App() {
   let pubRoutes, dashboardRoutes = [];
-  if (process.env.REACT_APP_ENV !== 'dev') {
+  if (process.env.REACT_APP_ENV === 'dev') {
     pubRoutes = [...publicRoutes, ...common];
     dashboardRoutes = [...protectedRoutes, ...common];
   } else {
