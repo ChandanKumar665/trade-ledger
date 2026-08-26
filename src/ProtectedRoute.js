18d9e0df87a20782e485e878a07df40773ccc46b
ck6652265 <ck6652265@gmail.com>
2026-08-26 19:56:30 +0530

    fixes

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -3,7 +3,6 @@ import { useAuth } from "../src/hooks/useAuth";
 
 export default function ProtectedRoute({ children }) {
     const { user } = useAuth();
-    console.log('protected user', user);
     return user
         ? children
         : <Navigate to="/login" replace />;
--- src/PublicRoute.js
+++ src/PublicRoute.js
@@ -3,7 +3,6 @@ import { useAuth } from "../src/hooks/useAuth";
 
 export default function PublicRoute({ children }) {
     const { user } = useAuth();
-    console.log('public', user)
     return user
         ? <Navigate to="/dashboard" replace />
         : children;
